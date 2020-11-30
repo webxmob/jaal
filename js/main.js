c77faf5d0c93015bb7f3374e110dd17e4cf2a5d6
Ali Tahir <57714516+Ali-Tahir4024@users.noreply.github.com>
2020-11-30 15:26:18 +0500

    updated the typography and some other things

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
-document.querySelector(".nav-menu").classList.add("nav-menu-active");
+document.querySelector('.nav-menu').classList.add('nav-menu-active');
 
 function navMenuOpen() {
-  document.querySelector(".nav-menu").classList.remove("nav-menu-active");
+  document.querySelector('.nav-menu').classList.remove('nav-menu-active');
 }
 
 function navMenuClose() {
-  document.querySelector(".nav-menu").classList.add("nav-menu-active");
+  document.querySelector('.nav-menu').classList.add('nav-menu-active');
 }
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY >= 100) {
+    document
+      .getElementsByClassName('navbar')[0]
+      .classList.add('navbar-scrolled');
+    document;
+    document
+      .getElementsByClassName('nav-menu-btn')[0]
+      .classList.add('onscroll-color');
+  } else {
+    document
+      .getElementsByClassName('navbar')[0]
+      .classList.remove('navbar-scrolled');
+    document
+      .getElementsByClassName('nav-menu-btn')[0]
+      .classList.remove('onscroll-color');
+  }
+});
