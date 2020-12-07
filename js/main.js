ed3828546f6bad864799255de3adace4e9a9fa57
Ali Tahir <57714516+Ali-Tahir4024@users.noreply.github.com>
2020-12-07 13:50:13 +0500

    removed the nav menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,4 @@
-document.querySelector('.nav-menu').classList.add('nav-menu-active');
-
-function navMenuOpen() {
-  document.querySelector('.nav-menu').classList.remove('nav-menu-active');
-}
-
-function navMenuClose() {
-  document.querySelector('.nav-menu').classList.add('nav-menu-active');
-}
+// navbar scroll effect
 
 window.addEventListener('scroll', () => {
   if (window.scrollY >= 100) {
